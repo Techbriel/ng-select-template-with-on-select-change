3486eff689ac589e188589abc9a5ee7f9a72cff4
Techbriel <106325196+Techbriel@users.noreply.github.com>
2022-09-25 23:05:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng-select-template-with-on-select-change
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng-select-template-error-vaj9ye)
---
+++ src/multi-select-custom-example.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DataService } from '../data.service';
+
+@Component({
+    selector: 'multi-select-custom-example',
+    templateUrl: './multi-select-custom-example.component.html',
+    styleUrls: ['./multi-select-custom-example.component.scss']
+})
+export class MultiSelectCustomExampleComponent implements OnInit {
+    condition = true;
+    githubUsers$: Observable<any[]>;
+    selectedUsers = [];
+    virtueSelected: boolean = false
+
+    constructor(private dataService: DataService) {
+    }
+
+    ngOnInit() {
+        this.githubUsers$ = this.dataService.getGithubAccounts('anjm');
+
+        console.log(this.selectedUsers, 'this is a selected user')
+    }
+
+    
+    onCarSelect(eventData: any){
+        if(eventData == 'anjm'){
+            this.virtueSelected == true
+            console.log('virtueSelected is true now')
+        }
+      console.log("CHANGE EVENT DATA:", eventData);
+      console.log(this.selectedUsers, 'new user selected')
+      
+    }
+    
+}
